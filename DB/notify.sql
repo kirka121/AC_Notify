a4550e045159461cfafaab188020c8afcbc00ad3
Kirill Afan <kirka121@gmail.com>
2013-06-01 13:02:44 -0400

    minor fixes. readme.

--- DB/notify.sql
+++ DB/notify.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `devices`
+--
+
+DROP TABLE IF EXISTS `devices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `devices` (
+  `device_id` varchar(45) NOT NULL,
+  `device_location` varchar(45) DEFAULT NULL,
+  `device_is_custom_profile` int(11) DEFAULT NULL,
+  `device_profile` int(11) DEFAULT NULL,
+  `custom_profile_html` longtext,
+  PRIMARY KEY (`device_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `devices`
+--
+
+LOCK TABLES `devices` WRITE;
+/*!40000 ALTER TABLE `devices` DISABLE KEYS */;
+INSERT INTO `devices` VALUES ('bfd37b493c922758','somewhere',0,1,'null');
+/*!40000 ALTER TABLE `devices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `profiles`
 --
 
@@ -39,6 +66,59 @@ LOCK TABLES `profiles` WRITE;
 INSERT INTO `profiles` VALUES (1,'some content here',0),(2,'some more content here',0);
 /*!40000 ALTER TABLE `profiles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `user_permission_sets`
+--
+
+DROP TABLE IF EXISTS `user_permission_sets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_permission_sets` (
+  `permission_set_id` int(11) NOT NULL AUTO_INCREMENT,
+  `manage_tablets` int(11) DEFAULT NULL,
+  `manage_users` int(11) DEFAULT NULL,
+  `manage_permissions` int(11) DEFAULT NULL,
+  PRIMARY KEY (`permission_set_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_permission_sets`
+--
+
+LOCK TABLES `user_permission_sets` WRITE;
+/*!40000 ALTER TABLE `user_permission_sets` DISABLE KEYS */;
+INSERT INTO `user_permission_sets` VALUES (1,1,1,1);
+/*!40000 ALTER TABLE `user_permission_sets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(45) DEFAULT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `permission_set` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'kirka','kirill','afan',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -49,4 +129,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-05-29 11:38:36
+-- Dump completed on 2013-06-01 12:20:06
